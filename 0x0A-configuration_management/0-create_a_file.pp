8feb0eb2ba55c0689283d90a622505587c8922ca
holic65 <okormark@gmail.com>
2022-10-18 16:11:02 -0700

    Task 0 push

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# create a file
+
+file { '/tmp/school':
+  ensure  => 'present'
+  replace => 'no',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
