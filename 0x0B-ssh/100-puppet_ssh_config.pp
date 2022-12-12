6bd1be1ca0413920709e82aa48fbd24e3933a683
holic65 <okormark@gmail.com>
2022-12-11 18:30:19 -0800

    advanced task

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,9 @@
+# a script that configures ssh file
+
+exec { 'Turn off passwd auth':
+  command => '/usr/bin/echo "PasswordAuthentication no" >> /etc/ssh/ssh_config'
+}
+
+exec { 'Declare identity file':
+  command => '/usr/bin/echo "IdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config'
+}
