83d7bbeea93acb56a2fc28afb32954de8aa480ae
holic65 <okormark@gmail.com>
2022-12-11 16:32:24 -0800

    task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -4,8 +4,8 @@ file { '/tmp/school':
   ensure  => 'present',
   replace => 'no',
   path    => '/tmp/school',
-  mode	  => '0744',
-  owner	  => 'www-data',
-  group	  => 'www-data',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
   content => 'I love Puppet',
 }
