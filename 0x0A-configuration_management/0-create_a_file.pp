5be5700a2d9e8c95559bb12838ecb505c16aad92
holic65 <okormark@gmail.com>
2022-12-11 16:29:36 -0800

    task 0

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,11 @@
+# a puppet script that creates a file in tmp
+
+file { '/tmp/school':
+  ensure  => 'present',
+  replace => 'no',
+  path    => '/tmp/school',
+  mode	  => '0744',
+  owner	  => 'www-data',
+  group	  => 'www-data',
+  content => 'I love Puppet',
+}
